fb94f38ac1cb82e9f70382d9abb73154e8632913
lorenzo <lorenzo.carrascosa@epitech.eu>
2022-01-30 19:53:54 +0100

    commit before redump

--- Hot_AI.py
+++ Hot_AI.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from numpy.core.fromnumeric import mean
 import torch
 import torchvision
 import torchvision.transforms as transforms
@@ -11,13 +12,16 @@ import torch.optim as optim
 
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 CLASSES = ('hot_dog', 'not_hot_dog')
 BATCH_SIZE = 10
+EPOCHS = 2
 ROOT_PATH = './dataset/'
+MODEL_PATH = './vgg16.pth'
 
 class Net(nn.Module):
     def __init__(self):
@@ -59,8 +63,8 @@ class Vgg16(nn.Module):
         self.conv13 = nn.Conv2d(512, 512, 3)
         self.fc1 = nn.Linear(512, 4096)
         self.fc2 = nn.Linear(4096, 4096)
-        self.fc3 = nn.Linear(4096, 4096)
-        self.fc4 = nn.Softmax(dim=1)
+        self.fc3 = nn.Linear(4096, 2)
+        # self.fc3 = nn.Softmax(dim=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -84,18 +88,31 @@ class Vgg16(nn.Module):
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.fc3(x)
         return x
 
-def load_data(root_path, batch_size):
+def load_train_data(root_path, batch_size):
+    
     transform = transforms.Compose([
         transforms.Resize(size=(224, 224)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     
-    data = datasets.ImageFolder(root=root_path, transform=transform)
+    data = datasets.ImageFolder(root=root_path + 'train/', transform=transform)
+    data_loader = torch.utils.data.DataLoader(dataset=data, batch_size=batch_size, shuffle=True)
+    
+    return data_loader
+
+def load_test_data(root_path, batch_size):
+    
+    transform = transforms.Compose([
+        transforms.Resize(size=(224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    
+    data = datasets.ImageFolder(root=root_path + 'test/', transform=transform)
     data_loader = torch.utils.data.DataLoader(dataset=data, batch_size=batch_size, shuffle=True)
     
     return data_loader
@@ -107,25 +124,52 @@ def imshow(img):
     plt.show()
 
 def main():
-    loader = load_data(ROOT_PATH, BATCH_SIZE)
+    train_loader = load_train_data(ROOT_PATH, BATCH_SIZE)
+    test_loader = load_test_data(ROOT_PATH, BATCH_SIZE)
 
     model = Vgg16()
+    model.to(device)
     
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    model.to(device)
     
-    for epoch in range(10):
-        for inputs, labels in loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-        print(f"Epoch {epoch+1}")
-    print('Finished Training')
+    if not os.path.isfile(MODEL_PATH):
+        mean_loss = 0.0
+        for epoch in range(EPOCHS):
+            for i, (inputs, labels) in enumerate(train_loader):
+                inputs, labels = inputs.to(device), labels.to(device)
     
+                optimizer.zero_grad()
+
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                mean_loss += loss.item()
+
+                loss.backward()
+                optimizer.step()
+
+                if (i+1) % 100 == 99:
+                    print(f"[{epoch+1}, {i+1}]: {mean_loss/100:.3f}")
+                    mean_loss = 0.0
+        print('Finished Training')
+        torch.save(model.state_dict(), MODEL_PATH)
+    else:
+        model.load_state_dict(torch.load(MODEL_PATH))
+        print('model loaded')
+    
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    
+    print(f'Accuracy of the network on the tests images: {100 * correct // total} %')
 
 if __name__ == '__main__':
     main()
